bbea5f8c75ee6e75a33398ff6364f692d168f5e8
renankrz <renankrz@gmail.com>
2022-06-08 16:55:23 -0300

    feat: point winner in the main diagonal

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,17 +11,25 @@ const playMove = (board, row, col, symbol) => {
 const isGameOver = (board, symbol) => {
   const rowsScore = new Array(board.length).fill(0);
   const colsScore = new Array(board.length).fill(0);
+  let mainDiagonalScore = 0;
 
   for (let row = 0; row < board.length; row++) {
     for (let col = 0; col < board.length; col++) {
       if (board[row][col] === symbol) {
         rowsScore[row]++;
         colsScore[col]++;
+        if (row === col) {
+          mainDiagonalScore++;
+        }
       }
     }
   }
 
-  if (rowsScore.includes(board.length) || colsScore.includes(board.length)) {
+  if (
+    rowsScore.includes(board.length) ||
+    colsScore.includes(board.length) ||
+    mainDiagonalScore === board.length
+  ) {
     return true;
   }
 
